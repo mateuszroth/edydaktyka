77e9fdd5820cf1b9f1c46601e9566204d3ef50d6
mateuszroth <mateusz.roth@gmail.com>
2019-09-07 18:41:01 +0200

    Add ESLint and Prettier

---
+++ backend/.prettierrc.js
@@ -0,0 +1,8 @@
+// based on https://dev.to/robertcoopercode/using-eslint-and-prettier-in-a-typescript-project-53jb
+module.exports = {
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 4,
+};
